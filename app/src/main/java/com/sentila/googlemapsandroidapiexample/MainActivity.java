f817e3992c8e6f03072360a10c0e13d3233b9e12
dhgkdud12 <dhgkdud14@gmail.com>
2021-09-03 22:05:22 +0900

    Google Map API

--- app/src/main/java/com/sentila/googlemapsandroidapiexample/MainActivity.java
+++ app/src/main/java/com/sentila/googlemapsandroidapiexample/MainActivity.java
@@ -1,7 +1,23 @@
 package com.sentila.googlemapsandroidapiexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -9,6 +25,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity
@@ -16,6 +33,10 @@ public class MainActivity extends AppCompatActivity
 
     private GoogleMap mMap;
 
+    Button btnLocation, btnKor2Loc;
+    EditText editText;
+
+    MarkerOptions myMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +46,160 @@ public class MainActivity extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //권한 설정
+        checkDangerousPermissions();
+
+        //객체 초기화
+        editText = findViewById(R.id.editText);
+        btnLocation = findViewById(R.id.button1);
+        btnKor2Loc = findViewById(R.id.button2);
+
+        //위치 확인 버튼 기능 추가
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestMyLocation();
+            }
+        });
+
+        btnKor2Loc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editText.getText().toString().length() > 0) {
+                    Location location = getLocationFromAddress(getApplicationContext(), editText.getText().toString());
+
+                    showCurrentLocation(location);
+                }
+            }
+        });
     }
+
+    private Location getLocationFromAddress(Context context, String address) {
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> addresses;
+        Location resLocation = new Location("");
+        try {
+            addresses = geocoder.getFromLocationName(address, 5);
+            if((addresses == null) || (addresses.size() == 0)) {
+                return null;
+            }
+            Address addressLoc = addresses.get(0);
+
+            resLocation.setLatitude(addressLoc.getLatitude());
+            resLocation.setLongitude(addressLoc.getLongitude());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resLocation;
+    }
+
+    private void requestMyLocation() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            long minTime = 1000;    //갱신 시간
+            float minDistance = 0;  //갱신에 필요한 최소 거리
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    showCurrentLocation(location);
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showCurrentLocation(Location location) {
+        LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+        String msg = "Latitutde : " + curPoint.latitude
+                + "\nLongitude : " + curPoint.longitude;
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        //화면 확대, 숫자가 클수록 확대
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+
+        //마커 찍기
+        Location targetLocation = new Location("");
+        targetLocation.setLatitude(37.4937);
+        targetLocation.setLongitude(127.0643);
+        showMyMarker(targetLocation);
+    }
+
+    //------------------권한 설정 시작------------------------
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_WIFI_STATE
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인됨.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+    //------------------권한 설정 끝------------------------
+
+
+
+    private void showMyMarker(Location location) {
+        if(myMarker == null) {
+            myMarker = new MarkerOptions();
+            myMarker.position(new LatLng(location.getLatitude(), location.getLongitude()));
+            myMarker.title("◎ 내위치\n");
+            myMarker.snippet("여기가 어디지?");
+            //myMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation));
+            mMap.addMarker(myMarker);
+        }
+    }
+
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
@@ -40,12 +214,7 @@ public class MainActivity extends AppCompatActivity
         markerOptions.snippet("한국의 수도");
         mMap.addMarker(markerOptions);
 
-
-        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
-
-        // CameraUpdateFactory.zoomTo가 오동작하네요.
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        //서울로 이동
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
 
 
