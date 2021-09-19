3d8c6f2ec19242cde95dec2f4516f67a7b557b2a
dhgkdud12 <dhgkdud14@gmail.com>
2021-09-19 17:27:50 +0900

    급식정보, 공지사항, 후원정보, 봉사정보 각 페이지 이동

---
+++ app/src/main/java/com/sentila/Mothershand/NoticeActivity.java
@@ -0,0 +1,13 @@
+package com.sentila.Mothershand;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoticeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notice_page);
+    }
+}
